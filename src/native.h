229f308c7f7d063fb38ca447f5e11c1b945a17ed
Mauricio-100 <ceoseshell@gmail.com>
2026-01-17 12:06:54 +0100

    Update native.h

--- src/native.h
+++ src/native.h
@@ -3,23 +3,9 @@
 
 #include "interpreter.h"
 
-// Fonctions natives
+// Prototypes des fonctions natives
 Value native_print(Value* args, int count, Environment* env);
-Value native_log(Value* args, int count, Environment* env);
-Value native_input(Value* args, int count, Environment* env);
-Value native_clock(Value* args, int count, Environment* env);
-Value native_typeof(Value* args, int count, Environment* env);
-Value native_length(Value* args, int count, Environment* env);
-Value native_range(Value* args, int count, Environment* env);
-Value native_map(Value* args, int count, Environment* env);
-Value native_filter(Value* args, int count, Environment* env);
-Value native_reduce(Value* args, int count, Environment* env);
 Value native_http_run(Value* args, int count, Environment* env);
-Value native_fs_read(Value* args, int count, Environment* env);
-Value native_fs_write(Value* args, int count, Environment* env);
-Value native_math_sqrt(Value* args, int count, Environment* env);
-Value native_math_pow(Value* args, int count, Environment* env);
-Value native_assert(Value* args, int count, Environment* env);
 
 // Enregistrement des fonctions natives
 void register_natives(Environment* env);
