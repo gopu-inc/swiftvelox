7d0432e3a005526c67181ffd36723260b75feb85
Mauricio-100 <ceoseshell@gmail.com>
2026-01-19 14:19:19 +0100

    Update common.h

--- common.h
+++ common.h
@@ -8,49 +8,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <math.h>
-#include <stdarg.h>
 
 // ======================================================
-// [SECTION] COULEURS POUR LE TERMINAL - AMÉLIORÉ
+// [SECTION] COULEURS POUR LE TERMINAL
 // ======================================================
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN    "\033[1;36m"
 #define RESET   "\033[0m"
-#define BOLD    "\033[1m"
-#define DIM     "\033[2m"
-#define ITALIC  "\033[3m"
-#define UNDERLINE "\033[4m"
-
-#define BLACK   "\033[30m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
-
-#define BG_BLACK   "\033[40m"
-#define BG_RED     "\033[41m"
-#define BG_GREEN   "\033[42m"
-#define BG_YELLOW  "\033[43m"
-#define BG_BLUE    "\033[44m"
-#define BG_MAGENTA "\033[45m"
-#define BG_CYAN    "\033[46m"
-#define BG_WHITE   "\033[47m"
-
-// Couleurs vives
-#define BRIGHT_BLACK   "\033[90m"
-#define BRIGHT_RED     "\033[91m"
-#define BRIGHT_GREEN   "\033[92m"
-#define BRIGHT_YELLOW  "\033[93m"
-#define BRIGHT_BLUE    "\033[94m"
-#define BRIGHT_MAGENTA "\033[95m"
-#define BRIGHT_CYAN    "\033[96m"
-#define BRIGHT_WHITE   "\033[97m"
-
-// Couleurs hexadécimales
-#define HEX_COLOR(r, g, b) "\033[38;2;" #r ";" #g ";" #b "m"
-#define BG_HEX_COLOR(r, g, b) "\033[48;2;" #r ";" #g ";" #b "m"
 
 // ======================================================
 // [SECTION] TOKEN DEFINITIONS
@@ -58,93 +26,48 @@
 typedef enum {
     // Literals
     TK_INT, TK_FLOAT, TK_STRING, TK_TRUE, TK_FALSE,
-    TK_NULL, TK_UNDEFINED,
-    
-    // JSON Types
-    TK_JSON_OBJECT, TK_JSON_ARRAY, TK_JSON_PAIR,
-    
+    TK_NULL,
     // Identifiers
     TK_IDENT,
-    
     // Operators
     TK_PLUS, TK_MINUS, TK_MULT, TK_DIV, TK_MOD,
-    TK_POW, TK_ROOT, TK_CONCAT,
-    
-    // Assignment
-    TK_ASSIGN, TK_PLUS_ASSIGN, TK_MINUS_ASSIGN,
-    TK_MULT_ASSIGN, TK_DIV_ASSIGN, TK_MOD_ASSIGN,
-    
-    // Comparison
-    TK_EQ, TK_NEQ, TK_GT, TK_LT, TK_GTE, TK_LTE,
+    TK_POW, TK_CONCAT,
+    TK_ASSIGN, TK_EQ, TK_NEQ, TK_GT, TK_LT, TK_GTE, TK_LTE,
     TK_AND, TK_OR, TK_NOT,
-    TK_SPACESHIP, // ===
-    
-    // Bitwise
-    TK_BIT_AND, TK_BIT_OR, TK_BIT_XOR, TK_BIT_NOT,
-    TK_SHL, TK_SHR, // << >>
-    
-    // Arrow operators
-    TK_LARROW,    // <
-    TK_RARROW,    // >
-    TK_DARROW,    // =>
-    TK_LDARROW,   // <==
-    TK_RDARROW,   // ==>
-    
-    // Range operators
-    TK_ELLIPSIS,  // ...
-    TK_RANGE,     // ..
-    TK_SCOPE,     // ::
-    
     // Punctuation
     TK_LPAREN, TK_RPAREN, TK_LBRACE, TK_RBRACE,
     TK_LBRACKET, TK_RBRACKET,
     TK_COMMA, TK_SEMICOLON, TK_COLON, TK_PERIOD,
-    TK_AT, TK_HASH, TK_DOLLAR, TK_QUESTION,
-    TK_PIPE, TK_AMPERSAND, TK_TILDE, TK_CARET,
-    
+    TK_AT, TK_HASH, TK_DOLLAR,
     // Keywords
     TK_VAR, TK_LET, TK_CONST,
     TK_NET, TK_CLOG, TK_DOS, TK_SEL,
-    
     // Control flow
-    TK_IF, TK_ELSE, TK_ELIF, TK_THEN,
+    TK_IF, TK_ELSE, TK_ELIF,
     TK_WHILE, TK_FOR, TK_DO,
     TK_SWITCH, TK_CASE, TK_DEFAULT,
     TK_BREAK, TK_CONTINUE, TK_RETURN,
-    
     // Functions & Modules
     TK_FUNC, TK_IMPORT, TK_EXPORT, TK_FROM,
     TK_CLASS, TK_STRUCT, TK_ENUM, TK_TYPEDEF,
     TK_TYPELOCK, TK_NAMESPACE,
-    TK_PUBLIC, TK_PRIVATE, TK_PROTECTED,
-    
     // Types
     TK_TYPE_INT, TK_TYPE_FLOAT, TK_TYPE_STR,
     TK_TYPE_BOOL, TK_TYPE_CHAR, TK_TYPE_VOID,
     TK_TYPE_ANY, TK_TYPE_AUTO,
-    TK_TYPE_JSON, TK_TYPE_ARRAY,
-    
-    // Special types
     TK_TYPE_NET, TK_TYPE_CLOG, TK_TYPE_DOS,
-    TK_TYPE_SEL, TK_TYPE_MODULE,
-    
+    TK_TYPE_SEL,
     // Memory & Size
     TK_SIZEOF, TK_SIZE, TK_SIZ,
     TK_NEW, TK_DELETE, TK_FREE,
-    TK_ALLOC, TK_REALLOC,
-    
     // Debug & DB
-    TK_DB, TK_DBVAR, TK_PRINT_DB, TK_TRACE,
-    
+    TK_DB, TK_DBVAR, TK_PRINT_DB,
     // I/O
-    TK_PRINT, TK_INPUT, TK_READ, TK_WRITE,
-    
+    TK_PRINT, TK_INPUT,
     // Special
-    TK_MAIN, TK_THIS, TK_SUPER, TK_SELF,
-    TK_INIT, TK_DEINIT, TK_CONSTRUCTOR, TK_DESTRUCTOR,
-    
+    TK_MAIN, TK_THIS, TK_SELF,
     // End markers
-    TK_EOF, TK_ERROR, TK_UNKNOWN
+    TK_EOF, TK_ERROR
 } TokenKind;
 
 // Token structure
@@ -171,15 +94,13 @@ typedef struct {
 static const Keyword keywords[] = {
     // Variables
     {"var", TK_VAR}, {"let", TK_LET}, {"const", TK_CONST},
-    {"net", TK_NET}, {"clog", TK_CLOG}, {"dos", TK_DOS},
-    {"sel", TK_SEL},
+    {"net", TK_NET}, {"clog", TK_CLOG}, {"dos", TK_DOS}, {"sel", TK_SEL},
     
     // Control flow
     {"if", TK_IF}, {"else", TK_ELSE}, {"elif", TK_ELIF},
-    {"then", TK_THEN}, {"while", TK_WHILE}, {"for", TK_FOR}, 
-    {"do", TK_DO}, {"switch", TK_SWITCH}, {"case", TK_CASE}, 
-    {"default", TK_DEFAULT}, {"break", TK_BREAK}, {"continue", TK_CONTINUE}, 
-    {"return", TK_RETURN},
+    {"while", TK_WHILE}, {"for", TK_FOR}, {"do", TK_DO},
+    {"switch", TK_SWITCH}, {"case", TK_CASE}, {"default", TK_DEFAULT},
+    {"break", TK_BREAK}, {"continue", TK_CONTINUE}, {"return", TK_RETURN},
     
     // Functions
     {"func", TK_FUNC}, {"import", TK_IMPORT}, {"export", TK_EXPORT},
@@ -187,82 +108,31 @@ static const Keyword keywords[] = {
     {"enum", TK_ENUM}, {"typedef", TK_TYPEDEF}, {"typelock", TK_TYPELOCK},
     {"namespace", TK_NAMESPACE},
     
-    // Access modifiers
-    {"public", TK_PUBLIC}, {"private", TK_PRIVATE}, {"protected", TK_PROTECTED},
-    
     // Types
     {"int", TK_TYPE_INT}, {"float", TK_TYPE_FLOAT}, {"string", TK_TYPE_STR},
     {"bool", TK_TYPE_BOOL}, {"char", TK_TYPE_CHAR}, {"void", TK_TYPE_VOID},
     {"any", TK_TYPE_ANY}, {"auto", TK_TYPE_AUTO},
-    {"json", TK_TYPE_JSON}, {"array", TK_TYPE_ARRAY},
-    {"netvar", TK_TYPE_NET}, {"clogvar", TK_TYPE_CLOG}, {"dosvar", TK_TYPE_DOS},
-    {"selvar", TK_TYPE_SEL}, {"module", TK_TYPE_MODULE},
+    {"netvar", TK_TYPE_NET}, {"clogvar", TK_TYPE_CLOG}, 
+    {"dosvar", TK_TYPE_DOS}, {"selvar", TK_TYPE_SEL},
     
     // Memory
     {"sizeof", TK_SIZEOF}, {"size", TK_SIZE}, {"siz", TK_SIZ},
     {"new", TK_NEW}, {"delete", TK_DELETE}, {"free", TK_FREE},
-    {"alloc", TK_ALLOC}, {"realloc", TK_REALLOC},
     
     // Debug
     {"db", TK_DB}, {"dbvar", TK_DBVAR}, {"printdb", TK_PRINT_DB},
-    {"trace", TK_TRACE},
     
     // I/O
-    {"print", TK_PRINT}, {"input", TK_INPUT}, {"read", TK_READ},
-    {"write", TK_WRITE},
+    {"print", TK_PRINT}, {"input", TK_INPUT},
     
     // Special
-    {"main", TK_MAIN}, {"this", TK_THIS}, {"super", TK_SUPER},
-    {"self", TK_SELF}, {"init", TK_INIT}, {"deinit", TK_DEINIT},
-    {"constructor", TK_CONSTRUCTOR}, {"destructor", TK_DESTRUCTOR},
+    {"main", TK_MAIN}, {"this", TK_THIS}, {"self", TK_SELF},
     
     // Literals
-    {"true", TK_TRUE}, {"false", TK_FALSE},
-    {"null", TK_NULL}, {"undefined", TK_UNDEFINED},
+    {"true", TK_TRUE}, {"false", TK_FALSE}, {"null", TK_NULL},
     
     // End marker
     {NULL, TK_ERROR}
-};
-
-// ======================================================
-// [SECTION] JSON SUPPORT
-// ======================================================
-typedef enum {
-    JSON_NULL,
-    JSON_BOOL,
-    JSON_INT,
-    JSON_FLOAT,
-    JSON_STRING,
-    JSON_ARRAY,
-    JSON_OBJECT
-} JsonType;
-
-typedef struct JsonValue JsonValue;
-typedef struct JsonPair JsonPair;
-
-struct JsonValue {
-    JsonType type;
-    union {
-        bool bool_val;
-        int64_t int_val;
-        double float_val;
-        char* str_val;
-        struct {
-            JsonValue* items;
-            int count;
-            int capacity;
-        } array;
-        struct {
-            JsonPair* pairs;
-            int count;
-            int capacity;
-        } object;
-    } value;
-};
-
-struct JsonPair {
-    char* key;
-    JsonValue* value;
 };
 
 // ======================================================
@@ -276,46 +146,17 @@ typedef enum {
     NODE_BOOL,
     NODE_IDENT,
     NODE_NULL,
-    NODE_UNDEFINED,
-    
-    // JSON Nodes
-    NODE_JSON_OBJECT,
-    NODE_JSON_ARRAY,
-    NODE_JSON_PAIR,
     
     // Operations
     NODE_BINARY,
     NODE_UNARY,
-    NODE_TERNARY,
     NODE_ASSIGN,
-    NODE_COMPOUND_ASSIGN,
     
     // Control flow
     NODE_IF,
-    NODE_IF_ELSE,
-    NODE_ELIF,
     NODE_WHILE,
     NODE_FOR,
-    NODE_DO_WHILE,
-    NODE_SWITCH,
-    NODE_CASE,
-    NODE_BREAK,
-    NODE_CONTINUE,
     NODE_RETURN,
-    
-    // Functions
-    NODE_FUNC_DECL,
-    NODE_FUNC_CALL,
-    NODE_LAMBDA,
-    
-    // Classes & Structs
-    NODE_CLASS,
-    NODE_STRUCT,
-    NODE_CLASS_INSTANCE,
-    NODE_MEMBER_ACCESS,
-    NODE_METHOD_CALL,
-    NODE_CONSTRUCTOR,
-    NODE_DESTRUCTOR,
     
     // Variables
     NODE_VAR_DECL,
@@ -323,62 +164,33 @@ typedef enum {
     NODE_CLOG_DECL,
     NODE_DOS_DECL,
     NODE_SEL_DECL,
-    NODE_CONST_DECL,
     
     // Memory
     NODE_SIZEOF,
-    NODE_ALLOC,
-    NODE_FREE,
     
     // Modules
     NODE_IMPORT,
-    NODE_EXPORT,
-    NODE_MODULE,
     
     // Debug
     NODE_DBVAR,
-    NODE_PRINT_DB,
-    NODE_TRACE,
     
     // I/O
     NODE_PRINT,
-    NODE_INPUT,
     
     // Blocks
     NODE_BLOCK,
-    NODE_PROGRAM,
     
     // Special
     NODE_MAIN,
-    NODE_TYPELOCK,
-    NODE_NAMESPACE,
     NODE_EMPTY
 } NodeType;
-
-// Function parameter
-typedef struct FuncParam {
-    char* name;
-    char* type;
-    struct FuncParam* next;
-} FuncParam;
-
-// Class member
-typedef struct ClassMember {
-    char* name;
-    char* type;
-    char* visibility; // public, private, protected
-    bool is_static;
-    bool is_const;
-    struct ClassMember* next;
-} ClassMember;
 
 // AST Node structure
 typedef struct ASTNode {
     NodeType type;
     struct ASTNode* left;
     struct ASTNode* right;
-    struct ASTNode* third; // For ternary or other 3-operand nodes
-    struct ASTNode* extra; // For additional data
+    struct ASTNode* third;
     
     union {
         // Basic values
@@ -393,27 +205,6 @@ typedef struct ASTNode {
         // Types
         char* type_name;
         
-        // JSON data
-        JsonValue* json_value;
-        
-        // Function/Class data
-        struct {
-            char* name;
-            FuncParam* params;
-            char* return_type;
-            struct ASTNode* body;
-            int param_count;
-        } func;
-        
-        struct {
-            char* name;
-            ClassMember* members;
-            struct ASTNode* constructor;
-            struct ASTNode* destructor;
-            int member_count;
-            char* parent_class;
-        } class_def;
-        
         // Import/Export
         struct {
             char** modules;
@@ -427,63 +218,19 @@ typedef struct ASTNode {
             int size_bytes;
         } size_info;
         
-        // For/While loops
+        // For loop
         struct {
             struct ASTNode* init;
             struct ASTNode* condition;
             struct ASTNode* update;
-            struct ASTNode* body;
         } loop;
     } data;
     
     // Additional info
     int line;
     int column;
-    TokenKind op_type; // For operators
-    bool is_exported;
-    bool is_global;
+    TokenKind op_type;
 } ASTNode;
-
-// ======================================================
-// [SECTION] SYMBOL TABLE
-// ======================================================
-typedef enum {
-    SYM_VARIABLE,
-    SYM_FUNCTION,
-    SYM_CLASS,
-    SYM_MODULE,
-    SYM_NAMESPACE,
-    SYM_PARAMETER,
-    SYM_MEMBER
-} SymbolType;
-
-typedef struct Symbol {
-    char* name;
-    SymbolType type;
-    char* data_type;
-    int size_bytes;
-    int scope_level;
-    int line_declared;
-    bool is_initialized;
-    bool is_constant;
-    bool is_exported;
-    union {
-        int64_t int_val;
-        double float_val;
-        char* str_val;
-        bool bool_val;
-        JsonValue* json_val;
-        ASTNode* func_node;
-        ASTNode* class_node;
-    } value;
-    struct Symbol* next;
-} Symbol;
-
-typedef struct Scope {
-    int level;
-    Symbol* symbols;
-    struct Scope* parent;
-} Scope;
 
 // ======================================================
 // [SECTION] HELPER FUNCTIONS
@@ -506,47 +253,4 @@ static inline char* str_ncopy(const char* src, int n) {
     return dest;
 }
 
-static inline char* str_format(const char* fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    
-    // Determine size needed
-    va_list args_copy;
-    va_copy(args_copy, args);
-    int size = vsnprintf(NULL, 0, fmt, args_copy) + 1;
-    va_end(args_copy);
-    
-    if (size <= 0) {
-        va_end(args);
-        return NULL;
-    }
-    
-    char* buffer = malloc(size);
-    if (buffer) {
-        vsnprintf(buffer, size, fmt, args);
-    }
-    
-    va_end(args);
-    return buffer;
-}
-
-// ======================================================
-// [SECTION] DBVAR TABLE STRUCTURE
-// ======================================================
-typedef struct {
-    char* name;
-    char* type;
-    int size_bytes;
-    char* value_str;
-    bool is_initialized;
-    int line;
-    int scope;
-} DBVarEntry;
-
-typedef struct {
-    DBVarEntry* entries;
-    int count;
-    int capacity;
-} DBVarTable;
-
-#endif // COMMON_H
+#endif
