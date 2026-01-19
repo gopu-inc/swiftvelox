9f424691b44ec1977f20d492116db5d6782c655b
Mauricio-100 <ceoseshell@gmail.com>
2026-01-19 13:49:18 +0100

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -107,12 +107,6 @@ static Token makeFloatToken(double value) {
     return token;
 }
 
-static Token makeBoolToken(bool value) {
-    Token token = makeToken(value ? TK_TRUE : TK_FALSE);
-    token.value.bool_val = value;
-    return token;
-}
-
 // ======================================================
 // [SECTION] SKIP WHITESPACE & COMMENTS
 // ======================================================
@@ -263,12 +257,6 @@ static Token number() {
     if (is_hex) {
         // Parse hexadecimal
         while (isxdigit(peek())) advance();
-        
-        if (peek() == '.') {
-            is_float = true;
-            advance(); // Consume '.'
-            while (isxdigit(peek())) advance();
-        }
     } else if (is_binary) {
         // Parse binary
         while (peek() == '0' || peek() == '1') advance();
@@ -283,14 +271,6 @@ static Token number() {
         if (peek() == '.' && isdigit(peekNext())) {
             is_float = true;
             advance(); // Consume '.'
-            while (isdigit(peek())) advance();
-        }
-        
-        // Scientific notation
-        if (peek() == 'e' || peek() == 'E') {
-            is_float = true;
-            advance(); // Consume 'e' or 'E'
-            if (peek() == '+' || peek() == '-') advance();
             while (isdigit(peek())) advance();
         }
     }
@@ -354,12 +334,6 @@ static Token identifier() {
             }
         }
         
-        // Check for special multi-word keywords
-        if (strcmp(text, "print") == 0) {
-            free(text);
-            return makeToken(TK_PRINT);
-        }
-        
         // If not a keyword, it's an identifier
         Token token = makeToken(TK_IDENT);
         token.value.str_val = text;
@@ -375,7 +349,7 @@ static Token identifier() {
 // ======================================================
 static Token operatorLexer() {
     char c = peek();
-    char error_msg[32]; // Déclaré en haut de la fonction
+    char error_msg[32];
     
     // Multi-character operators
     switch (c) {
@@ -562,204 +536,4 @@ Token scanToken() {
     
     // Operators and punctuation
     return operatorLexer();
-}
-
-// ======================================================
-// [SECTION] HELPER FUNCTIONS FOR DEBUGGING
-// ======================================================
-const char* tokenKindToString(TokenKind kind) {
-    switch (kind) {
-        case TK_INT: return "TK_INT";
-        case TK_FLOAT: return "TK_FLOAT";
-        case TK_STRING: return "TK_STRING";
-        case TK_TRUE: return "TK_TRUE";
-        case TK_FALSE: return "TK_FALSE";
-        case TK_NULL: return "TK_NULL";
-        case TK_UNDEFINED: return "TK_UNDEFINED";
-        
-        case TK_IDENT: return "TK_IDENT";
-        
-        // Operators
-        case TK_PLUS: return "TK_PLUS";
-        case TK_MINUS: return "TK_MINUS";
-        case TK_MULT: return "TK_MULT";
-        case TK_DIV: return "TK_DIV";
-        case TK_MOD: return "TK_MOD";
-        case TK_POW: return "TK_POW";
-        case TK_ROOT: return "TK_ROOT";
-        case TK_CONCAT: return "TK_CONCAT";
-        
-        // Assignment
-        case TK_ASSIGN: return "TK_ASSIGN";
-        case TK_PLUS_ASSIGN: return "TK_PLUS_ASSIGN";
-        case TK_MINUS_ASSIGN: return "TK_MINUS_ASSIGN";
-        case TK_MULT_ASSIGN: return "TK_MULT_ASSIGN";
-        case TK_DIV_ASSIGN: return "TK_DIV_ASSIGN";
-        case TK_MOD_ASSIGN: return "TK_MOD_ASSIGN";
-        
-        // Comparison
-        case TK_EQ: return "TK_EQ";
-        case TK_NEQ: return "TK_NEQ";
-        case TK_GT: return "TK_GT";
-        case TK_LT: return "TK_LT";
-        case TK_GTE: return "TK_GTE";
-        case TK_LTE: return "TK_LTE";
-        case TK_AND: return "TK_AND";
-        case TK_OR: return "TK_OR";
-        case TK_NOT: return "TK_NOT";
-        case TK_SPACESHIP: return "TK_SPACESHIP";
-        
-        // Bitwise
-        case TK_BIT_AND: return "TK_BIT_AND";
-        case TK_BIT_OR: return "TK_BIT_OR";
-        case TK_BIT_XOR: return "TK_BIT_XOR";
-        case TK_BIT_NOT: return "TK_BIT_NOT";
-        case TK_SHL: return "TK_SHL";
-        case TK_SHR: return "TK_SHR";
-        
-        // Arrow
-        case TK_LARROW: return "TK_LARROW";
-        case TK_RARROW: return "TK_RARROW";
-        case TK_DARROW: return "TK_DARROW";
-        case TK_LDARROW: return "TK_LDARROW";
-        case TK_RDARROW: return "TK_RDARROW";
-        
-        // Range
-        case TK_ELLIPSIS: return "TK_ELLIPSIS";
-        case TK_RANGE: return "TK_RANGE";
-        case TK_SCOPE: return "TK_SCOPE";
-        
-        // Punctuation
-        case TK_LPAREN: return "TK_LPAREN";
-        case TK_RPAREN: return "TK_RPAREN";
-        case TK_LBRACE: return "TK_LBRACE";
-        case TK_RBRACE: return "TK_RBRACE";
-        case TK_LBRACKET: return "TK_LBRACKET";
-        case TK_RBRACKET: return "TK_RBRACKET";
-        case TK_COMMA: return "TK_COMMA";
-        case TK_SEMICOLON: return "TK_SEMICOLON";
-        case TK_COLON: return "TK_COLON";
-        case TK_PERIOD: return "TK_PERIOD";
-        case TK_AT: return "TK_AT";
-        case TK_HASH: return "TK_HASH";
-        case TK_DOLLAR: return "TK_DOLLAR";
-        case TK_QUESTION: return "TK_QUESTION";
-        case TK_PIPE: return "TK_PIPE";
-        case TK_AMPERSAND: return "TK_AMPERSAND";
-        case TK_TILDE: return "TK_TILDE";
-        case TK_CARET: return "TK_CARET";
-        
-        // Keywords
-        case TK_VAR: return "TK_VAR";
-        case TK_LET: return "TK_LET";
-        case TK_CONST: return "TK_CONST";
-        case TK_NET: return "TK_NET";
-        case TK_CLOG: return "TK_CLOG";
-        case TK_DOS: return "TK_DOS";
-        case TK_SEL: return "TK_SEL";
-        
-        // Control flow
-        case TK_IF: return "TK_IF";
-        case TK_ELSE: return "TK_ELSE";
-        case TK_ELIF: return "TK_ELIF";
-        case TK_WHILE: return "TK_WHILE";
-        case TK_FOR: return "TK_FOR";
-        case TK_DO: return "TK_DO";
-        case TK_SWITCH: return "TK_SWITCH";
-        case TK_CASE: return "TK_CASE";
-        case TK_DEFAULT: return "TK_DEFAULT";
-        case TK_BREAK: return "TK_BREAK";
-        case TK_CONTINUE: return "TK_CONTINUE";
-        case TK_RETURN: return "TK_RETURN";
-        
-        // Functions & Modules
-        case TK_FUNC: return "TK_FUNC";
-        case TK_IMPORT: return "TK_IMPORT";
-        case TK_EXPORT: return "TK_EXPORT";
-        case TK_FROM: return "TK_FROM";
-        case TK_CLASS: return "TK_CLASS";
-        case TK_STRUCT: return "TK_STRUCT";
-        case TK_ENUM: return "TK_ENUM";
-        case TK_TYPEDEF: return "TK_TYPEDEF";
-        case TK_TYPELOCK: return "TK_TYPELOCK";
-        case TK_NAMESPACE: return "TK_NAMESPACE";
-        case TK_PUBLIC: return "TK_PUBLIC";
-        case TK_PRIVATE: return "TK_PRIVATE";
-        case TK_PROTECTED: return "TK_PROTECTED";
-        
-        // Types
-        case TK_TYPE_INT: return "TK_TYPE_INT";
-        case TK_TYPE_FLOAT: return "TK_TYPE_FLOAT";
-        case TK_TYPE_STR: return "TK_TYPE_STR";
-        case TK_TYPE_BOOL: return "TK_TYPE_BOOL";
-        case TK_TYPE_CHAR: return "TK_TYPE_CHAR";
-        case TK_TYPE_VOID: return "TK_TYPE_VOID";
-        case TK_TYPE_ANY: return "TK_TYPE_ANY";
-        case TK_TYPE_AUTO: return "TK_TYPE_AUTO";
-        case TK_TYPE_NET: return "TK_TYPE_NET";
-        case TK_TYPE_CLOG: return "TK_TYPE_CLOG";
-        case TK_TYPE_DOS: return "TK_TYPE_DOS";
-        case TK_TYPE_SEL: return "TK_TYPE_SEL";
-        case TK_TYPE_MODULE: return "TK_TYPE_MODULE";
-        
-        // Memory
-        case TK_SIZEOF: return "TK_SIZEOF";
-        case TK_SIZE: return "TK_SIZE";
-        case TK_SIZ: return "TK_SIZ";
-        case TK_NEW: return "TK_NEW";
-        case TK_DELETE: return "TK_DELETE";
-        case TK_FREE: return "TK_FREE";
-        case TK_ALLOC: return "TK_ALLOC";
-        case TK_REALLOC: return "TK_REALLOC";
-        
-        // Debug
-        case TK_DB: return "TK_DB";
-        case TK_DBVAR: return "TK_DBVAR";
-        case TK_PRINT_DB: return "TK_PRINT_DB";
-        case TK_TRACE: return "TK_TRACE";
-        
-        // I/O
-        case TK_PRINT: return "TK_PRINT";
-        case TK_INPUT: return "TK_INPUT";
-        case TK_READ: return "TK_READ";
-        case TK_WRITE: return "TK_WRITE";
-        
-        // Special
-        case TK_MAIN: return "TK_MAIN";
-        case TK_THIS: return "TK_THIS";
-        case TK_SUPER: return "TK_SUPER";
-        case TK_SELF: return "TK_SELF";
-        case TK_INIT: return "TK_INIT";
-        case TK_DEINIT: return "TK_DEINIT";
-        case TK_CONSTRUCTOR: return "TK_CONSTRUCTOR";
-        case TK_DESTRUCTOR: return "TK_DESTRUCTOR";
-        
-        // End markers
-        case TK_EOF: return "TK_EOF";
-        case TK_ERROR: return "TK_ERROR";
-        case TK_UNKNOWN: return "TK_UNKNOWN";
-        
-        default: return "TK_UNKNOWN";
-    }
-}
-
-void printToken(Token token) {
-    printf("[TOKEN] Line %d, Col %d: %s '", 
-           token.line, token.column, tokenKindToString(token.kind));
-    
-    if (token.kind == TK_STRING && token.value.str_val) {
-        printf("%s", token.value.str_val);
-    } else if (token.kind == TK_INT) {
-        printf("%lld", token.value.int_val);
-    } else if (token.kind == TK_FLOAT) {
-        printf("%f", token.value.float_val);
-    } else if (token.kind == TK_TRUE || token.kind == TK_FALSE) {
-        printf("%s", token.value.bool_val ? "true" : "false");
-    } else {
-        for (int i = 0; i < token.length; i++) {
-            putchar(token.start[i]);
-        }
-    }
-    
-    printf("'\n");
 }
