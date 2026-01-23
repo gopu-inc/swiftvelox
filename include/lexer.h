6e5e8ca076deddfd277af17558e914b8c3a405f8
Mauricio-100 <ceoseshell@gmail.com>
2026-01-23 03:55:20 +0100

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -3,23 +3,18 @@
 
 #include "common.h"
 
-// Structure déjà déclarée dans common.h, on utilise une déclaration forward
-struct Lexer;
-
-// Fonctions lexer
-void lexer_init(struct Lexer* lexer, const char* source, const char* filename);
-Token lexer_next_token(struct Lexer* lexer);
-Token lexer_peek_token(struct Lexer* lexer);
-void lexer_skip_whitespace(struct Lexer* lexer);
-bool lexer_is_at_end(struct Lexer* lexer);
-char lexer_advance(struct Lexer* lexer);
-char lexer_peek(struct Lexer* lexer);
-char lexer_peek_next(struct Lexer* lexer);
-bool lexer_match(struct Lexer* lexer, char expected);
-Token lexer_make_token(struct Lexer* lexer, TokenKind kind);
-Token lexer_error_token(struct Lexer* lexer, const char* message);
-Token lexer_string(struct Lexer* lexer);
-Token lexer_number(struct Lexer* lexer);
-Token lexer_identifier(struct Lexer* lexer);
+// Fonctions lexer seulement
+Token lexer_peek_token(Lexer* lexer);
+void lexer_skip_whitespace(Lexer* lexer);
+bool lexer_is_at_end(Lexer* lexer);
+char lexer_advance(Lexer* lexer);
+char lexer_peek(Lexer* lexer);
+char lexer_peek_next(Lexer* lexer);
+bool lexer_match(Lexer* lexer, char expected);
+Token lexer_make_token(Lexer* lexer, TokenKind kind);
+Token lexer_error_token(Lexer* lexer, const char* message);
+Token lexer_string(Lexer* lexer);
+Token lexer_number(Lexer* lexer);
+Token lexer_identifier(Lexer* lexer);
 
 #endif // LEXER_H
