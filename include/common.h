353aa1f179d3e5a863a672c177ea36353a3f44b1
Mauricio-100 <ceoseshell@gmail.com>
2026-01-23 20:08:40 +0100

    Update common.h

--- include/common.h
+++ include/common.h
@@ -1,16 +1,11 @@
-
-
-
-
-
-/*
-[file name]: src/common.h
-[file content begin]
-// swiftflow v3.2.1 powered by gopu.inc
-*/
 #ifndef COMMON_H
 #define COMMON_H
 
+// swiftflow v3.2.1 powered by gopu.inc
+
+// ======================================================
+// [SECTION] HEADERS STANDARD
+// ======================================================
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -24,7 +19,7 @@
 #include <errno.h>
 #include <time.h>
 #include <math.h>
-
+#include <ctype.h>
 
 // ======================================================
 // [SECTION] VERSION & CONFIGURATION
@@ -209,81 +204,7 @@ typedef struct {
     int length;
 } Keyword;
 
-static const Keyword keywords[] = {
-    // Variables
-    {"var", TK_VAR, 3}, {"let", TK_LET, 3}, {"const", TK_CONST, 5},
-    {"net", TK_NET, 3}, {"clog", TK_CLOG, 4}, {"dos", TK_DOS, 3}, {"sel", TK_SEL, 3},
-    
-    // Control flow
-    {"if", TK_IF, 2}, {"else", TK_ELSE, 4}, {"elif", TK_ELIF, 4},
-    {"while", TK_WHILE, 5}, {"for", TK_FOR, 3}, {"do", TK_DO, 2},
-    {"switch", TK_SWITCH, 6}, {"case", TK_CASE, 4}, {"default", TK_DEFAULT, 7},
-    {"break", TK_BREAK, 5}, {"continue", TK_CONTINUE, 8}, {"return", TK_RETURN, 6},
-    {"then", TK_THEN, 4}, {"yield", TK_YIELD, 5},
-    
-    // Error handling
-    {"try", TK_TRY, 3}, {"catch", TK_CATCH, 5}, {"finally", TK_FINALLY, 7},
-    {"throw", TK_THROW, 5},
-    
-    // Functions
-    {"func", TK_FUNC, 4}, {"import", TK_IMPORT, 6}, {"export", TK_EXPORT, 6},
-    {"from", TK_FROM, 4}, {"class", TK_CLASS, 5}, {"struct", TK_STRUCT, 6},
-    {"enum", TK_ENUM, 4}, {"interface", TK_INTERFACE, 9}, {"typedef", TK_TYPEDEF, 7},
-    {"typelock", TK_TYPELOCK, 8}, {"namespace", TK_NAMESPACE, 9},
-    
-    // Types
-    {"int", TK_TYPE_INT, 3}, {"float", TK_TYPE_FLOAT, 5}, {"string", TK_TYPE_STR, 6},
-    {"bool", TK_TYPE_BOOL, 4}, {"char", TK_TYPE_CHAR, 4}, {"void", TK_TYPE_VOID, 4},
-    {"any", TK_TYPE_ANY, 3}, {"auto", TK_TYPE_AUTO, 4}, {"unknown", TK_TYPE_UNKNOWN, 7},
-    {"netvar", TK_TYPE_NET, 6}, {"clogvar", TK_TYPE_CLOG, 7}, 
-    {"dosvar", TK_TYPE_DOS, 6}, {"selvar", TK_TYPE_SEL, 6},
-    {"array", TK_TYPE_ARRAY, 5}, {"map", TK_TYPE_MAP, 3}, {"func", TK_TYPE_FUNC, 4},
-    
-    // Memory
-    {"sizeof", TK_SIZEOF, 6}, {"size", TK_SIZE, 4}, {"siz", TK_SIZ, 3},
-    {"new", TK_NEW, 3}, {"delete", TK_DELETE, 6}, {"free", TK_FREE, 4},
-    
-    // Debug
-    {"db", TK_DB, 2}, {"dbvar", TK_DBVAR, 5}, {"printdb", TK_PRINT_DB, 7},
-    {"assert", TK_ASSERT, 6},
-    
-    // I/O
-    {"print", TK_PRINT, 5}, {"weld", TK_WELD, 4}, {"read", TK_READ, 4},
-    {"write", TK_WRITE, 5}, {"input", TK_INPUT, 5},
-    
-    // New keywords
-    {"pass", TK_PASS, 4}, {"global", TK_GLOBAL, 6}, {"lambda", TK_LAMBDA, 6},
-    {"bdd", TK_BDD, 3}, {"def", TK_DEF, 3}, {"type", TK_TYPE, 4}, 
-    {"raise", TK_RAISE, 5}, {"with", TK_WITH, 4}, {"learn", TK_LEARN, 5},
-    {"nonlocal", TK_NONLOCAL, 8}, {"lock", TK_LOCK, 4}, {"append", TK_APPEND, 6},
-    {"push", TK_PUSH, 4}, {"pop", TK_POP, 3}, {"to", TK_TO, 2},
-    
-    // JSON & Data
-    {"json", TK_JSON, 4}, {"yaml", TK_YAML, 4}, {"xml", TK_XML, 3},
-    
-    // Operators as keywords
-    {"in", TK_IN, 2}, {"is", TK_IS, 2}, {"isnot", TK_ISNOT, 5}, {"as", TK_AS_OP, 2},
-    {"of", TK_OF, 2},
-    
-    // Special
-    {"main", TK_MAIN, 4}, {"this", TK_THIS, 4}, {"self", TK_SELF, 4},
-    {"super", TK_SUPER, 5}, {"static", TK_STATIC, 6}, {"public", TK_PUBLIC, 6},
-    {"private", TK_PRIVATE, 7}, {"protected", TK_PROTECTED, 9},
-    
-    // Async
-    {"async", TK_ASYNC, 5}, {"await", TK_AWAIT, 5},
-    
-    // File operations
-    {"open", TK_FILE_OPEN, 4}, {"close", TK_FILE_CLOSE, 5},
-    {"fread", TK_FILE_READ, 5}, {"fwrite", TK_FILE_WRITE, 6},
-    
-    // Literals
-    {"true", TK_TRUE, 4}, {"false", TK_FALSE, 5}, {"null", TK_NULL, 4},
-    {"undefined", TK_UNDEFINED, 9}, {"nan", TK_NAN, 3}, {"inf", TK_INF, 3},
-    
-    // End marker
-    {NULL, TK_ERROR, 0}
-};
+extern const Keyword keywords[];
 
 // ======================================================
 // [SECTION] LEXER STRUCTURE
@@ -358,9 +279,6 @@ struct Value {
     } as;
 };
 
-// ======================================================
-// [SECTION] AST NODE DEFINITIONS
-// ======================================================
 // ======================================================
 // [SECTION] AST NODE DEFINITIONS
 // ======================================================
@@ -542,6 +460,7 @@ typedef struct ASTNode {
     int column;
     TokenKind op_type;
 } ASTNode;
+
 // ======================================================
 // [SECTION] INTERPRETER STRUCTURES
 // ======================================================
@@ -607,6 +526,24 @@ typedef struct {
 } SwiftFlowConfig;
 
 // ======================================================
+// [SECTION] BACKEND STRUCTURES
+// ======================================================
+typedef enum {
+    BACKEND_LLVM,
+    BACKEND_NASM,
+    BACKEND_INTERPRETER
+} BackendTarget;
+
+typedef struct {
+    BackendTarget target;
+    FILE* output_file;
+    char* output_filename;
+    int label_counter;
+    int temp_counter;
+    void* symbol_table;
+} BackendContext;
+
+// ======================================================
 // [SECTION] HELPER FUNCTIONS
 // ======================================================
 static inline char* str_copy(const char* src) {
@@ -664,6 +601,13 @@ static inline bool str_endswith(const char* str, const char* suffix) {
     return strcmp(str + str_len - suffix_len, suffix) == 0;
 }
 
+static inline bool str_startswith(const char* str, const char* prefix) {
+    if (!str || !prefix) return false;
+    size_t prefix_len = strlen(prefix);
+    if (prefix_len > strlen(str)) return false;
+    return strncmp(str, prefix, prefix_len) == 0;
+}
+
 // Memory management helper
 #define ALLOC(type) ((type*)malloc(sizeof(type)))
 #define ALLOC_ARRAY(type, count) ((type*)malloc(sizeof(type) * (count)))
@@ -698,23 +642,8 @@ static inline bool str_endswith(const char* str, const char* suffix) {
 // ======================================================
 // [SECTION] FUNCTION PROTOTYPES
 // ======================================================
-// Ajouter dans la section FUNCTION PROTOTYPES:
-bool config_add_import_path(SwiftFlowConfig* config, const char* path);
-char* config_resolve_import(SwiftFlowConfig* config, const char* module_name, const char* from_file);
-Value swiftflow_json_parse(const char* json_str);
-char* swiftflow_json_stringify(Value value);
+
 // Logging
-// Ajouter ces prototypes dans common.h:
-
-// Server functions
-void start_http_server(int port, const char* host, bool dev_mode);
-
-// JSON library function
-Value swiftflow_json_read_file(const char* filename);
-bool swiftflow_json_write_file(const char* filename, Value value);
-
-// Utility functions
-bool str_startswith(const char* str, const char* prefix);
 void swiftflow_log(LogLevel level, const char* file, int line, const char* fmt, ...);
 
 // AST functions
@@ -756,6 +685,7 @@ Value value_make_nan(void);
 Value value_make_inf(void);
 void value_free(Value* value);
 char* value_to_string(Value value);
+char* value_to_raw_string(Value value);
 void value_print(Value value);
 bool value_is_truthy(Value value);
 bool value_equal(Value a, Value b);
@@ -778,6 +708,7 @@ void interpreter_error(SwiftFlowInterpreter* interpreter, const char* message, i
 void interpreter_register_builtins(SwiftFlowInterpreter* interpreter);
 void interpreter_dump_environment(SwiftFlowInterpreter* interpreter);
 void interpreter_dump_value(Value value);
+Value call_builtin(SwiftFlowInterpreter* interpreter, const char* name, Value* args, int arg_count);
 
 // Lexer functions
 void lexer_init(Lexer* lexer, const char* source, const char* filename);
@@ -799,7 +730,6 @@ Token lexer_identifier(Lexer* lexer);
 void parser_init(Parser* parser, Lexer* lexer);
 ASTNode* parse_program(Parser* parser);
 ASTNode* parse_statement(Parser* parser);
-// * ASTNode* parse_expression(Parser* parser); *//
 ASTNode* parse_block(Parser* parser);
 void parser_synchronize(Parser* parser);
 bool parser_match(Parser* parser, TokenKind kind);
@@ -807,15 +737,13 @@ bool parser_check(Parser* parser, TokenKind kind);
 Token parser_consume(Parser* parser, TokenKind kind, const char* error_message);
 void parser_error(Parser* parser, Token token, const char* message);
 void parser_error_at_current(Parser* parser, const char* message);
+Token parser_advance(Parser* parser);
 ASTNode* parse_import_statement(Parser* parser);
 ASTNode* parse_if_statement(Parser* parser);
 ASTNode* parse_while_statement(Parser* parser);
 ASTNode* parse_for_statement(Parser* parser);
 ASTNode* parse_var_declaration(Parser* parser);
 ASTNode* parse_function_declaration(Parser* parser);
-ASTNode* parse_class_declaration(Parser* parser);
-ASTNode* parse_switch_statement(Parser* parser);
-ASTNode* parse_try_statement(Parser* parser);
 ASTNode* parse_print_statement(Parser* parser);
 ASTNode* parse_input_statement(Parser* parser);
 
@@ -825,8 +753,41 @@ void config_free(SwiftFlowConfig* config);
 bool config_add_import_path(SwiftFlowConfig* config, const char* path);
 char* config_resolve_import(SwiftFlowConfig* config, const char* module_name, const char* from_file);
 
-#endif // COMMON_H
+// Backend functions
+BackendContext* backend_create(BackendTarget target, const char* filename);
+void backend_free(BackendContext* context);
+void backend_compile(BackendContext* context, ASTNode* ast);
+void llvm_emit_prologue(BackendContext* context);
+void llvm_emit_epilogue(BackendContext* context);
+void llvm_emit_node(BackendContext* context, ASTNode* node);
 
-/*
-    [file content end]
-*/
+// JSON functions
+Value swiftflow_json_parse(const char* json_str);
+char* swiftflow_json_stringify(Value value);
+Value swiftflow_json_read_file(const char* filename);
+bool swiftflow_json_write_file(const char* filename, Value value);
+Value json_builtin_parse(SwiftFlowInterpreter* interpreter, Value* args, int arg_count);
+Value json_builtin_stringify(SwiftFlowInterpreter* interpreter, Value* args, int arg_count);
+Value json_builtin_read_file(SwiftFlowInterpreter* interpreter, Value* args, int arg_count);
+Value json_builtin_write_file(SwiftFlowInterpreter* interpreter, Value* args, int arg_count);
+
+// HTTP server
+void start_http_server(int port, const char* host, bool dev_mode);
+
+// Math functions (if using libtommath)
+#ifdef HAVE_TOMMATH
+void mathlib_register(SwiftFlowInterpreter* interpreter);
+#endif
+
+// Regex functions (if using pcre)
+#ifdef HAVE_PCRE
+void regexlib_register(SwiftFlowInterpreter* interpreter);
+#endif
+
+// REPL functions
+#ifdef HAVE_READLINE
+void swiftflow_repl(SwiftFlowInterpreter* interpreter);
+#endif
+void swiftflow_simple_repl(SwiftFlowInterpreter* interpreter);
+
+#endif // COMMON_H
