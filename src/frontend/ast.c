b345f93f154a40176daba6061ee2aa893642d358
Mauricio-100 <ceoseshell@gmail.com>
2026-01-23 15:25:11 +0100

    Update ast.c

--- src/frontend/ast.c
+++ src/frontend/ast.c
@@ -52,8 +52,6 @@ ASTNode* ast_new_binary(NodeType type, ASTNode* left, ASTNode* right, int line, 
     ASTNode* node = ast_new_node(type, line, column);
     node->left = left;
     node->right = right;
-    node->data.binary_op.left = left;
-    node->data.binary_op.right = right;
     return node;
 }
 
@@ -117,15 +115,12 @@ ASTNode* ast_new_for(ASTNode* init, ASTNode* condition, ASTNode* update, ASTNode
     return node;
 }
 
-// Create function node
+// Create function node (simplifié)
 ASTNode* ast_new_function(const char* name, ASTNode* params, ASTNode* body, int line, int column) {
     ASTNode* node = ast_new_node(NODE_FUNC, line, column);
     node->data.func_def.name = str_copy(name);
     node->data.func_def.params = params;
     node->data.func_def.body = body;
-    node->data.func_def.return_type = NULL;
-    node->data.func_def.is_async = false;
-    node->data.func_def.is_generator = false;
     return node;
 }
 
@@ -134,14 +129,16 @@ ASTNode* ast_new_function_call(ASTNode* function, ASTNode* args, int line, int c
     ASTNode* node = ast_new_node(NODE_FUNC_CALL, line, column);
     node->data.func_call.function = function;
     node->data.func_call.arguments = args;
+    
     // Count arguments
     int count = 0;
     ASTNode* arg = args;
     while (arg) {
         count++;
-        arg = arg->right; // Arguments are stored as a linked list
+        arg = arg->right;
     }
     node->data.func_call.arg_count = count;
+    
     return node;
 }
 
@@ -152,13 +149,27 @@ ASTNode* ast_new_return(ASTNode* value, int line, int column) {
     return node;
 }
 
-// Create import node
+// Create import node (simplifié)
 ASTNode* ast_new_import(char** modules, int count, const char* from_module, int line, int column) {
     ASTNode* node = ast_new_node(NODE_IMPORT, line, column);
-    node->data.imports.modules = modules;
-    node->data.imports.module_count = count;
-    node->data.imports.from_module = str_copy(from_module);
-    node->data.imports.is_selective = (count > 0);
+    
+    // Prend seulement le premier module pour simplifier
+    if (count > 0 && modules[0]) {
+        node->data.import_info.module_name = str_copy(modules[0]);
+    } else {
+        node->data.import_info.module_name = NULL;
+    }
+    
+    node->data.import_info.from_module = from_module ? str_copy(from_module) : NULL;
+    
+    // Libérer le tableau de modules
+    if (modules) {
+        for (int i = 0; i < count; i++) {
+            FREE(modules[i]);
+        }
+        FREE(modules);
+    }
+    
     return node;
 }
 
@@ -172,7 +183,7 @@ ASTNode* ast_new_print(ASTNode* value, int line, int column) {
 // Create input node
 ASTNode* ast_new_input(const char* prompt, int line, int column) {
     ASTNode* node = ast_new_node(NODE_INPUT, line, column);
-    node->data.input_op.prompt = str_copy(prompt);
+    node->data.input_op.prompt = prompt ? str_copy(prompt) : NULL;
     return node;
 }
 
@@ -196,16 +207,10 @@ void ast_free(ASTNode* node) {
             FREE(node->data.func_def.name);
             ast_free(node->data.func_def.params);
             ast_free(node->data.func_def.body);
-            ast_free(node->data.func_def.return_type);
             break;
         case NODE_IMPORT:
-            if (node->data.imports.modules) {
-                for (int i = 0; i < node->data.imports.module_count; i++) {
-                    FREE(node->data.imports.modules[i]);
-                }
-                FREE(node->data.imports.modules);
-            }
-            FREE(node->data.imports.from_module);
+            FREE(node->data.import_info.module_name);
+            FREE(node->data.import_info.from_module);
             break;
         case NODE_INPUT:
             FREE(node->data.input_op.prompt);
@@ -237,7 +242,7 @@ void ast_print(ASTNode* node, int indent) {
     
     switch (node->type) {
         case NODE_INT:
-            printf(": %ld\n", node->data.int_val);
+            printf(": %lld\n", (long long)node->data.int_val);
             break;
         case NODE_FLOAT:
             printf(": %f\n", node->data.float_val);
@@ -265,6 +270,16 @@ void ast_print(ASTNode* node, int indent) {
             printf(": %s\n", node->data.func_def.name);
             ast_print(node->data.func_def.params, indent + 1);
             ast_print(node->data.func_def.body, indent + 1);
+            break;
+        case NODE_IMPORT:
+            printf(": import ");
+            if (node->data.import_info.module_name) {
+                printf("%s", node->data.import_info.module_name);
+            }
+            if (node->data.import_info.from_module) {
+                printf(" from %s", node->data.import_info.from_module);
+            }
+            printf("\n");
             break;
         case NODE_IF:
             printf("\n");
@@ -376,189 +391,67 @@ const char* node_type_to_string(NodeType type) {
 
 // Convert token kind to string
 const char* token_kind_to_string(TokenKind kind) {
-    switch (kind) {
-        case TK_INT: return "INT";
-        case TK_FLOAT: return "FLOAT";
-        case TK_STRING: return "STRING";
-        case TK_TRUE: return "TRUE";
-        case TK_FALSE: return "FALSE";
-        case TK_NULL: return "NULL";
-        case TK_UNDEFINED: return "UNDEFINED";
-        case TK_NAN: return "NAN";
-        case TK_INF: return "INF";
-        case TK_IDENT: return "IDENT";
-        case TK_AS: return "AS";
-        case TK_OF: return "OF";
-        case TK_PLUS: return "PLUS";
-        case TK_MINUS: return "MINUS";
-        case TK_MULT: return "MULT";
-        case TK_DIV: return "DIV";
-        case TK_MOD: return "MOD";
-        case TK_POW: return "POW";
-        case TK_CONCAT: return "CONCAT";
-        case TK_SPREAD: return "SPREAD";
-        case TK_NULLISH: return "NULLISH";
-        case TK_ASSIGN: return "ASSIGN";
-        case TK_EQ: return "EQ";
-        case TK_NEQ: return "NEQ";
-        case TK_GT: return "GT";
-        case TK_LT: return "LT";
-        case TK_GTE: return "GTE";
-        case TK_LTE: return "LTE";
-        case TK_PLUS_ASSIGN: return "PLUS_ASSIGN";
-        case TK_MINUS_ASSIGN: return "MINUS_ASSIGN";
-        case TK_MULT_ASSIGN: return "MULT_ASSIGN";
-        case TK_DIV_ASSIGN: return "DIV_ASSIGN";
-        case TK_MOD_ASSIGN: return "MOD_ASSIGN";
-        case TK_POW_ASSIGN: return "POW_ASSIGN";
-        case TK_CONCAT_ASSIGN: return "CONCAT_ASSIGN";
-        case TK_AND: return "AND";
-        case TK_OR: return "OR";
-        case TK_NOT: return "NOT";
-        case TK_BIT_AND: return "BIT_AND";
-        case TK_BIT_OR: return "BIT_OR";
-        case TK_BIT_XOR: return "BIT_XOR";
-        case TK_BIT_NOT: return "BIT_NOT";
-        case TK_SHL: return "SHL";
-        case TK_SHR: return "SHR";
-        case TK_USHR: return "USHR";
-        case TK_RARROW: return "RARROW";
-        case TK_DARROW: return "DARROW";
-        case TK_LDARROW: return "LDARROW";
-        case TK_RDARROW: return "RDARROW";
-        case TK_SPACESHIP: return "SPACESHIP";
-        case TK_ELLIPSIS: return "ELLIPSIS";
-        case TK_RANGE: return "RANGE";
-        case TK_RANGE_INCL: return "RANGE_INCL";
-        case TK_QUESTION: return "QUESTION";
-        case TK_SCOPE: return "SCOPE";
-        case TK_SAFE_NAV: return "SAFE_NAV";
-        case TK_IN: return "IN";
-        case TK_IS: return "IS";
-        case TK_ISNOT: return "ISNOT";
-        case TK_AS_OP: return "AS_OP";
-        case TK_LPAREN: return "LPAREN";
-        case TK_RPAREN: return "RPAREN";
-        case TK_LBRACE: return "LBRACE";
-        case TK_RBRACE: return "RBRACE";
-        case TK_LBRACKET: return "LBRACKET";
-        case TK_RBRACKET: return "RBRACKET";
-        case TK_LSQUARE: return "LSQUARE";
-        case TK_RSQUARE: return "RSQUARE";
-        case TK_COMMA: return "COMMA";
-        case TK_SEMICOLON: return "SEMICOLON";
-        case TK_COLON: return "COLON";
-        case TK_PERIOD: return "PERIOD";
-        case TK_AT: return "AT";
-        case TK_HASH: return "HASH";
-        case TK_DOLLAR: return "DOLLAR";
-        case TK_BACKTICK: return "BACKTICK";
-        case TK_VAR: return "VAR";
-        case TK_LET: return "LET";
-        case TK_CONST: return "CONST";
-        case TK_NET: return "NET";
-        case TK_CLOG: return "CLOG";
-        case TK_DOS: return "DOS";
-        case TK_SEL: return "SEL";
-        case TK_THEN: return "THEN";
-        case TK_DO: return "DO";
-        case TK_IF: return "IF";
-        case TK_ELSE: return "ELSE";
-        case TK_ELIF: return "ELIF";
-        case TK_WHILE: return "WHILE";
-        case TK_FOR: return "FOR";
-        case TK_SWITCH: return "SWITCH";
-        case TK_CASE: return "CASE";
-        case TK_DEFAULT: return "DEFAULT";
-        case TK_BREAK: return "BREAK";
-        case TK_CONTINUE: return "CONTINUE";
-        case TK_RETURN: return "RETURN";
-        case TK_YIELD: return "YIELD";
-        case TK_TRY: return "TRY";
-        case TK_CATCH: return "CATCH";
-        case TK_FINALLY: return "FINALLY";
-        case TK_THROW: return "THROW";
-        case TK_FUNC: return "FUNC";
-        case TK_IMPORT: return "IMPORT";
-        case TK_EXPORT: return "EXPORT";
-        case TK_FROM: return "FROM";
-        case TK_CLASS: return "CLASS";
-        case TK_STRUCT: return "STRUCT";
-        case TK_ENUM: return "ENUM";
-        case TK_INTERFACE: return "INTERFACE";
-        case TK_TYPEDEF: return "TYPEDEF";
-        case TK_TYPELOCK: return "TYPELOCK";
-        case TK_NAMESPACE: return "NAMESPACE";
-        case TK_TYPE_INT: return "TYPE_INT";
-        case TK_TYPE_FLOAT: return "TYPE_FLOAT";
-        case TK_TYPE_STR: return "TYPE_STR";
-        case TK_TYPE_BOOL: return "TYPE_BOOL";
-        case TK_TYPE_CHAR: return "TYPE_CHAR";
-        case TK_TYPE_VOID: return "TYPE_VOID";
-        case TK_TYPE_ANY: return "TYPE_ANY";
-        case TK_TYPE_AUTO: return "TYPE_AUTO";
-        case TK_TYPE_UNKNOWN: return "TYPE_UNKNOWN";
-        case TK_TYPE_NET: return "TYPE_NET";
-        case TK_TYPE_CLOG: return "TYPE_CLOG";
-        case TK_TYPE_DOS: return "TYPE_DOS";
-        case TK_TYPE_SEL: return "TYPE_SEL";
-        case TK_TYPE_ARRAY: return "TYPE_ARRAY";
-        case TK_TYPE_MAP: return "TYPE_MAP";
-        case TK_TYPE_FUNC: return "TYPE_FUNC";
-        case TK_DECREMENT: return "DECREMENT";
-        case TK_INCREMENT: return "INCREMENT";
-        case TK_TYPEOF: return "TYPEOF";
-        case TK_SIZEOF: return "SIZEOF";
-        case TK_SIZE: return "SIZE";
-        case TK_SIZ: return "SIZ";
-        case TK_NEW: return "NEW";
-        case TK_DELETE: return "DELETE";
-        case TK_FREE: return "FREE";
-        case TK_DB: return "DB";
-        case TK_DBVAR: return "DBVAR";
-        case TK_PRINT_DB: return "PRINT_DB";
-        case TK_ASSERT: return "ASSERT";
-        case TK_PRINT: return "PRINT";
-        case TK_WELD: return "WELD";
-        case TK_READ: return "READ";
-        case TK_WRITE: return "WRITE";
-        case TK_INPUT: return "INPUT";
-        case TK_PASS: return "PASS";
-        case TK_GLOBAL: return "GLOBAL";
-        case TK_LAMBDA: return "LAMBDA";
-        case TK_BDD: return "BDD";
-        case TK_DEF: return "DEF";
-        case TK_TYPE: return "TYPE";
-        case TK_RAISE: return "RAISE";
-        case TK_WITH: return "WITH";
-        case TK_LEARN: return "LEARN";
-        case TK_NONLOCAL: return "NONLOCAL";
-        case TK_LOCK: return "LOCK";
-        case TK_APPEND: return "APPEND";
-        case TK_PUSH: return "PUSH";
-        case TK_POP: return "POP";
-        case TK_TO: return "TO";
-        case TK_JSON: return "JSON";
-        case TK_YAML: return "YAML";
-        case TK_XML: return "XML";
-        case TK_MAIN: return "MAIN";
-        case TK_THIS: return "THIS";
-        case TK_SELF: return "SELF";
-        case TK_SUPER: return "SUPER";
-        case TK_STATIC: return "STATIC";
-        case TK_PUBLIC: return "PUBLIC";
-        case TK_PRIVATE: return "PRIVATE";
-        case TK_PROTECTED: return "PROTECTED";
-        case TK_ASYNC: return "ASYNC";
-        case TK_AWAIT: return "AWAIT";
-        case TK_FILE_OPEN: return "FILE_OPEN";
-        case TK_FILE_CLOSE: return "FILE_CLOSE";
-        case TK_FILE_READ: return "FILE_READ";
-        case TK_FILE_WRITE: return "FILE_WRITE";
-        case TK_EOF: return "EOF";
-        case TK_ERROR: return "ERROR";
-        default: return "UNKNOWN_TOKEN";
+    // Tableau pour une recherche rapide
+    static const char* token_names[] = {
+        "INT", "FLOAT", "STRING", "TRUE", "FALSE",
+        "NULL", "UNDEFINED", "NAN", "INF",
+        "IDENT", "AS", "OF",
+        "PLUS", "MINUS", "MULT", "DIV", "MOD",
+        "POW", "CONCAT", "SPREAD", "NULLISH",
+        "ASSIGN", "EQ", "NEQ", "GT", "LT", "GTE", "LTE",
+        "PLUS_ASSIGN", "MINUS_ASSIGN", "MULT_ASSIGN", 
+        "DIV_ASSIGN", "MOD_ASSIGN", "POW_ASSIGN",
+        "CONCAT_ASSIGN",
+        "AND", "OR", "NOT",
+        "BIT_AND", "BIT_OR", "BIT_XOR", "BIT_NOT",
+        "SHL", "SHR", "USHR",
+        "RARROW", "DARROW", "LDARROW", "RDARROW",
+        "SPACESHIP", "ELLIPSIS", "RANGE", "RANGE_INCL",
+        "QUESTION", "SCOPE", "SAFE_NAV",
+        "IN", "IS", "ISNOT", "AS_OP",
+        "LPAREN", "RPAREN", "LBRACE", "RBRACE",
+        "LBRACKET", "RBRACKET",
+        "COMMA", "SEMICOLON", "COLON", "PERIOD",
+        "AT", "HASH", "DOLLAR", "BACKTICK",
+        "PIPE", "AMPERSAND", "CARET", "TILDE",
+        "EXCLAMATION", "QUESTION_MARK",
+        "LSQUARE", "RSQUARE",
+        "VAR", "LET", "CONST",
+        "NET", "CLOG", "DOS", "SEL",
+        "THEN", "DO",
+        "IF", "ELSE", "ELIF",
+        "WHILE", "FOR", "SWITCH", "CASE", "DEFAULT",
+        "BREAK", "CONTINUE", "RETURN", "YIELD",
+        "TRY", "CATCH", "FINALLY", "THROW",
+        "FUNC", "IMPORT", "EXPORT", "FROM",
+        "CLASS", "STRUCT", "ENUM", "INTERFACE",
+        "TYPEDEF", "TYPELOCK", "NAMESPACE",
+        "TYPE_INT", "TYPE_FLOAT", "TYPE_STR",
+        "TYPE_BOOL", "TYPE_CHAR", "TYPE_VOID",
+        "TYPE_ANY", "TYPE_AUTO", "TYPE_UNKNOWN",
+        "TYPE_NET", "TYPE_CLOG", "TYPE_DOS",
+        "TYPE_SEL", "TYPE_ARRAY", "TYPE_MAP",
+        "TYPE_FUNC", "DECREMENT", "INCREMENT", "TYPEOF",
+        "SIZEOF", "SIZE", "SIZ",
+        "NEW", "DELETE", "FREE",
+        "DB", "DBVAR", "PRINT_DB", "ASSERT",
+        "PRINT", "WELD", "READ", "WRITE", "INPUT",
+        "PASS", "GLOBAL", "LAMBDA",
+        "BDD", "DEF", "TYPE", "RAISE",
+        "WITH", "LEARN", "NONLOCAL", "LOCK", 
+        "APPEND", "PUSH", "POP", "TO",
+        "JSON", "YAML", "XML",
+        "MAIN", "THIS", "SELF", "SUPER",
+        "STATIC", "PUBLIC", "PRIVATE", "PROTECTED",
+        "ASYNC", "AWAIT",
+        "FILE_OPEN", "FILE_CLOSE", "FILE_READ", "FILE_WRITE",
+        "EOF", "ERROR"
+    };
+    
+    if (kind >= 0 && kind < sizeof(token_names) / sizeof(token_names[0])) {
+        return token_names[kind];
     }
+    return "UNKNOWN_TOKEN";
 }
 
 // AST Optimizer (simple version)
@@ -614,6 +507,9 @@ ASTNode* ast_optimize(ASTNode* node) {
                             node->data.int_val = result;
                             node->left = node->right = NULL;
                         }
+                        break;
+                    default:
+                        // Leave other operators unchanged
                         break;
                 }
             }
