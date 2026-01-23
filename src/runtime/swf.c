bc9887a66f1d5cf7fb3cc5eb12990f8cf07d6763
Mauricio-100 <ceoseshell@gmail.com>
2026-01-23 16:01:37 +0100

    Update swf.c

--- src/runtime/swf.c
+++ src/runtime/swf.c
@@ -93,7 +93,7 @@ char* config_resolve_import(SwiftFlowConfig* config, const char* module_name, co
     // Check if it's a standard library module
     if (str_startswith(module_name, "std:")) {
         // Standard library import
-        char* stdlib_path = "/usr/local/share/swiftflow/stdlib/";
+        char* stdlib_path = "/usr/local/lib/swift/";
         const char* module_path = module_name + 4; // Skip "std:"
         resolved = str_format("%s%s.swf", stdlib_path, module_path);
         
@@ -138,9 +138,9 @@ char* config_resolve_import(SwiftFlowConfig* config, const char* module_name, co
     
     // Check default locations
     const char* default_paths[] = {
-        "/usr/local/lib/swiftflow/",
-        "/usr/lib/swiftflow/",
-        "~/.swiftflow/modules/",
+        "/usr/local/lib/swift/",
+        "/usr/lib/swift/",
+        "~/.swift/modules/",
         "./modules/",
         NULL
     };
@@ -154,118 +154,4 @@ char* config_resolve_import(SwiftFlowConfig* config, const char* module_name, co
     }
     
     return NULL;
-}
-
-// Value creation functions
-Value value_make_int(int64_t val) {
-    Value value;
-    value.type = VAL_INT;
-    value.as.int_val = val;
-    return value;
-}
-
-Value value_make_float(double val) {
-    Value value;
-    value.type = VAL_FLOAT;
-    value.as.float_val = val;
-    return value;
-}
-
-Value value_make_bool(bool val) {
-    Value value;
-    value.type = VAL_BOOL;
-    value.as.bool_val = val;
-    return value;
-}
-
-Value value_make_string(const char* val) {
-    Value value;
-    value.type = VAL_STRING;
-    value.as.str_val = str_copy(val);
-    return value;
-}
-
-Value value_make_null(void) {
-    Value value;
-    value.type = VAL_NULL;
-    return value;
-}
-
-Value value_make_undefined(void) {
-    Value value;
-    value.type = VAL_UNDEFINED;
-    return value;
-}
-
-Value value_make_nan(void) {
-    Value value;
-    value.type = VAL_NAN;
-    value.as.float_val = NAN;
-    return value;
-}
-
-Value value_make_inf(void) {
-    Value value;
-    value.type = VAL_INF;
-    value.as.float_val = INFINITY;
-    return value;
-}
-
-void value_free(Value* value) {
-    if (!value) return;
-    
-    switch (value->type) {
-        case VAL_STRING:
-            FREE(value->as.str_val);
-            break;
-        case VAL_ARRAY:
-            if (value->as.array.elements) {
-                for (int i = 0; i < value->as.array.count; i++) {
-                    value_free(&value->as.array.elements[i]);
-                }
-                FREE(value->as.array.elements);
-            }
-            break;
-        case VAL_MAP:
-            if (value->as.map.keys) {
-                for (int i = 0; i < value->as.map.count; i++) {
-                    FREE(value->as.map.keys[i]);
-                    value_free(&value->as.map.values[i]);
-                }
-                FREE(value->as.map.keys);
-                FREE(value->as.map.values);
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-char* value_to_string(Value value) {
-    switch (value.type) {
-        case VAL_INT:
-            return str_format("%ld", value.as.int_val);
-        case VAL_FLOAT:
-            if (isnan(value.as.float_val)) {
-                return str_copy("nan");
-            }
-            if (isinf(value.as.float_val)) {
-                return str_copy(value.as.float_val > 0 ? "inf" : "-inf");
-            }
-            return str_format("%g", value.as.float_val);
-        case VAL_BOOL:
-            return str_copy(value.as.bool_val ? "true" : "false");
-        case VAL_STRING:
-            return str_format("\"%s\"", value.as.str_val);
-        case VAL_NULL:
-            return str_copy("null");
-        case VAL_UNDEFINED:
-            return str_copy("undefined");
-        case VAL_NAN:
-            return str_copy("nan");
-        case VAL_INF:
-            return str_copy("inf");
-        default:
-            return str_format("<value type %d>", value.type);
-    }
 }
