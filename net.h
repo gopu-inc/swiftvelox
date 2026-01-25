7961f36cbeec06c9975b6ba8149f89638f73cd9b
Mauricio-100 <ceoseshell@gmail.com>
2026-01-25 12:12:13 +0100

    Update net.h

--- net.h
+++ net.h
@@ -5,12 +5,14 @@
 #include "common.h"
 
 void init_net_module(void);
-void net_socket(ASTNode* node);
-void net_connect(ASTNode* node);
-void net_listen(ASTNode* node);
-void net_accept(ASTNode* node);
-void net_send(ASTNode* node);
-void net_recv(ASTNode* node);
-void net_close(ASTNode* node);
+
+// Nouvelles signatures avec types primitifs
+int net_socket_create(void);
+void net_connect_to(int fd, const char* ip, int port);
+int net_start_listen(int port);
+int net_accept_client(int server_fd);
+void net_send_data(int fd, const char* data);
+char* net_recv_data(int fd, int size);
+void net_close_socket(int fd);
 
 #endif
