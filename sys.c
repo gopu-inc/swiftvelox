6a140320a4a45ac83d1b8618c35daecdc89092ab
Mauricio-100 <ceoseshell@gmail.com>
2026-01-26 13:13:16 +0100

    Update sys.c

--- sys.c
+++ sys.c
@@ -11,29 +11,39 @@ static char** g_argv;
 void init_sys_module(int argc, char** argv) {
     g_argc = argc;
     g_argv = argv;
-    printf("%s[SYS MODULE]%s Initialized (%d args)\n", COLOR_CYAN, COLOR_RESET, argc);
+    // Debug pour voir ce qu'on reçoit
+    // printf("[SYS DEBUG] Argc: %d\n", argc);
+    // for(int i=0; i<argc; i++) printf("[SYS DEBUG] Arg[%d]: %s\n", i, argv[i]);
 }
 
-// Récupère l'argument à l'index donné.
-// Note : Index 0 correspond au premier argument APRES le nom du script
-// Usage : ./swift zarch.swf install (install = index 0)
 char* sys_get_argv(int index) {
-    // Offset de 2 : [0]=./swift, [1]=zarch.swf, [2]=Arg0
-    int real_idx = index + 2;
-    
-    if (real_idx < g_argc) {
-        return g_argv[real_idx]; // Retourne le pointeur direct (lecture seule)
+    // Logique intelligente pour trouver le premier argument après le script .swf
+    int script_idx = -1;
+    for (int i = 0; i < g_argc; i++) {
+        if (strstr(g_argv[i], ".swf")) {
+            script_idx = i;
+            break;
+        }
+    }
+
+    if (script_idx == -1) return NULL;
+
+    // index 0 = l'argument APRES le script
+    int target_idx = script_idx + 1 + index;
+
+    if (target_idx < g_argc) {
+        return g_argv[target_idx];
     }
     return NULL;
 }
 
-// Exécute une commande shell et retourne le code de sortie
 int sys_exec_int(const char* cmd) {
-    if (!cmd) return -1;
-    printf("%s[SYS EXEC]%s %s\n", COLOR_YELLOW, COLOR_RESET, cmd);
+    if (!cmd) {
+        printf("%s[SYS ERROR]%s Command is NULL\n", COLOR_RED, COLOR_RESET);
+        return -1;
+    }
+    // printf("%s[SYS EXEC]%s %s\n", COLOR_YELLOW, COLOR_RESET, cmd);
     int res = system(cmd);
-    
-    // WEXITSTATUS pour avoir le vrai code de retour Linux (0-255)
     if (res == -1) return -1;
     return (res >> 8) & 0xFF; 
 }
